6911c8c0cce0b2136818c1046c09b3235a4d412c
Pavel <pavel@krutov.eu>
2023-12-12 12:16:45 +0200

    Revert "implement all KD Tree methods fully"
    This reverts commit b607c905f22fab83982375189ce16786499a8a6c.

--- src/tree.py
+++ src/tree.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 from logs import log
 from src.entity import BaseEntity
 
@@ -7,7 +5,7 @@ from datetime import datetime
 
 
 class Node:
-    def __init__(self, data: tuple[float, float], depth: int, left_node, right_node):
+    def __init__(self, data: BaseEntity, depth: int, left_node, right_node):
         self.data = data
         self.left_child = left_node
         self.right_child = right_node
@@ -29,72 +27,61 @@ class Node:
 
 class KDTree:
     def __init__(self, points: list, depth: int = 0):
-        self.points = deepcopy(points)
         self.nodes = []
         self.queue = []
         start = datetime.now()
         self.__create_tree(points, depth)
         log(f"Tree of size {len(self.nodes)} created in {datetime.now() - start}")
 
-    def __create_tree(self, points: list[tuple[float, float]], depth: int = 0):
+    def __create_tree(self, entities: list[BaseEntity], depth: int = 0):
         axis = depth % 2
-        if len(points) != 0:
-            points.sort(key=lambda i: (i.x if axis == 0 else i.y))
-            middle_value = len(points) // 2
-            median_point = points[middle_value]
+        if len(entities) != 0:
+            entities.sort(key=lambda i: (i.x if axis == 0 else i.y))
+            middle_value = len(entities) // 2
+            median = entities[middle_value]
 
-            left = self.__create_tree(points[0:middle_value], depth + 1)
-            right = self.__create_tree(points[middle_value + 1:], depth + 1)
+            left = self.__create_tree(entities[0:middle_value], depth + 1)
+            right = self.__create_tree(entities[middle_value + 1:], depth + 1)
 
-            node = Node(median_point, depth, left, right)
+            node = Node(median, depth, left, right)
             self.nodes.insert(0, node)
             return node
 
     def find(self, point: tuple[float, float]) -> bool | BaseEntity:
         """
-        Checks through the points list to see if the point exists in the tree
+        Performs a Binary Search through the tree until it finds a node with
+        the specified coordinates or reaches a leaf node
         :param point:
         :return:
         """
-        return point in self.points
+        node: Node = self.nodes[0] if len(self.nodes) != 0 else None
+        depth = 0
+        found = False
+        leaf = False
+        while not (found or leaf):
+            axis = depth % 2
+            if node is None:
+                leaf = True
+                return False
+            elif node.data.get_coordinates() == point:
+                found = True
+                print(point)
+                return node.data
+            elif point[axis] <= node.data.get_coordinates()[axis]:
+                node = node.left_child
+            elif point[axis] >= node.data.get_coordinates()[axis]:
+                node = node.right_child
+            # There was a bug here where i didnt have the equals and it would enter a forever loop and crash. I fixed it.
+            # Bug where it does not properly find all of the points...
+            # I believe this is due to the fact that there is <= sign. There needs to be a separate == check, where it checks
+            # one subtree until it reaches a "dead end" (it cant possibly be any further), and then switches to the next subtree.
 
-    def range_search(self, point: tuple[float, float], topleft: tuple[float, float], bottomright: tuple[float, float]):
+    def range_search(self, node: Node, radius: float):
         """
         Performs a Breadth First Search through the tree, discarding subtrees that
         have no chance of falling within the box.
-        The box radius is the size of half the length of the box.
-        :param point:
-        :param topleft:
-        :param bottomright:
+        :param node:
+        :param radius:
         :return:
         """
-        points_list = []
-        self.queue.append(self.nodes[0])
-
-        while len(self.queue) != 0:
-            current_node: Node = self.queue.pop(0)
-            depth = current_node.depth
-            axis = depth % 2
-            opposite = (depth + 1) % 2
-
-            lb = topleft[axis] if axis == 0 else bottomright[axis]
-            ub = bottomright[axis] if axis == 0 else topleft[axis]
-
-            if lb <= current_node.data[axis] <= ub:
-                if current_node.left_child is not None:
-                    self.queue.append(current_node.left_child)
-                if current_node.right_child is not None:
-                    self.queue.append(current_node.right_child)
-
-                lb = topleft[opposite] if opposite == 0 else bottomright[opposite]
-                ub = bottomright[opposite] if opposite == 0 else topleft[opposite]
-
-                if lb <= current_node.data[opposite] <= ub:
-                    points_list.append(current_node.data)
-            else:
-                if point[axis] < current_node.data[axis] and current_node.left_child is not None:
-                    self.queue.append(current_node.left_child)
-                elif point[axis] > current_node.data[axis] and current_node.right_child is not None:
-                    self.queue.append(current_node.right_child)
-
-        return points_list
+        ...
