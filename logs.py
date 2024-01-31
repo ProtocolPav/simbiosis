c138f3d1209a6ff47692610ee573f0c0141a20db
Pavel <pavel@krutov.eu>
2024-01-31 10:53:02 +0200

    fix logging bug

--- logs.py
+++ logs.py
@@ -1,13 +1,17 @@
 from datetime import datetime
 
+time_now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+logfile = open(f"./logs/log-{time_now}.txt", "w")
+
+logfile.write(f"Start Simbiosis Simulation v0.4\n"
+              f"Start Time: {time_now}\n\n\n"
+              f"Runtime Logs:\n"
+              f"{'-' * 60}\n")
+
 
 def log(message: str):
-    time_now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-    logfile = open(f"./logs/log-{time_now}.txt", "w")
     print(f"[{datetime.now()}]", message)
     logfile.write(f"[{datetime.now()}] {message}\n")
-    logfile.close()
-
 
 # if __name__ == "__main__":
 #     time_now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
