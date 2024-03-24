79aa16c2cf15e58ebdf819fd3abbf3addf3a4819
Pavel <pbkrutov@gmail.com>
2024-03-24 12:57:36 +0200

    bump version number in logs

--- logs.py
+++ logs.py
@@ -3,7 +3,7 @@ from datetime import datetime
 time_now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 logfile = open(f"./logs/log-{time_now}.txt", "w")
 
-logfile.write(f"Start Simbiosis Simulation v0.4\n"
+logfile.write(f"Start Simbiosis Simulation v1.0\n"
               f"Start Time: {time_now}\n\n\n"
               f"Runtime Logs:\n"
               f"{'-' * 60}\n")
